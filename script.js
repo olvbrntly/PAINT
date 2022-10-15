e11abc6d1dbb325185afa987dbdd4794af8539a2
olive <olivia.brantley9@gmail.com>
2022-10-15 13:01:48 -0700

    minor updates and js outline

---
+++ script.js
@@ -0,0 +1,43 @@
+//VARIABLES---------------------------------
+const grid = document.createElement('div');
+let paintPanel = document.getElementById("paint-panel");
+
+//FUNCTIONS---------------------------------
+
+function createGrid(){
+
+}
+
+function colorBlack(){
+
+}
+
+function colorRainbow(){
+
+}
+
+function resetGrid(){
+
+}
+
+function erase(){
+    
+}
+
+//EVENT LISTENERS----------------------------------
+
+//click 8x8 to make grid 8x8
+
+//click 16x16 to make grid 8x8
+
+//click 32x32 to make grid 8x8
+
+//enter number make grid XxX
+
+//click make color black
+
+//click make color rainbow
+
+//click erase color
+
+//click reset to empty
